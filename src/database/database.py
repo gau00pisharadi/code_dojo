df41eb1d2dd38213622a9d0438ec75fccd83835f
gau00pisharadi <85791992+gau00pisharadi@users.noreply.github.com>
2026-07-17 08:36:03 +0530

    Create database.py

---
+++ src/database/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import sessionmaker
+
+from src.core.config import DATABASE_URL
+
+
+engine = create_engine(
+    DATABASE_URL,
+    echo=False,
+    future=True
+)
+
+
+SessionLocal = sessionmaker(
+    bind=engine,
+    autoflush=False,
+    autocommit=False
+)
+
+
+class Base(DeclarativeBase):
+    pass
