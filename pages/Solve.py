215fd496b19a9068c610a12cd992f811e50cc533
gau00pisharadi <85791992+gau00pisharadi@users.noreply.github.com>
2026-07-18 08:43:21 +0530

    Update Solve.py

--- pages/Solve.py
+++ pages/Solve.py
@@ -75,7 +75,7 @@ st.subheader("Your Solution")
 user_code = st_ace(
     value=problem["starter_code"],
     language="python",
-    theme="solarized_dark",
+    theme="terminal",
     height=350,
     key=f"editor_{problem['id']}",
 )
